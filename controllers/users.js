af3af6f7aa85663b9b710dec86c88b838526a2c3
jamesawp1 <jamesawp@protonmail.com>
2025-05-22 17:47:27 -0300

    refactor: user controller

--- controllers/users.js
+++ controllers/users.js
@@ -11,3 +11,11 @@ module.exports.getUserById = (req, res) => {
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(404).send({ message: err.message }));
 };
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(404).send({ data: err.message }));
+};
