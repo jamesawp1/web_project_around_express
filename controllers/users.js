b8ecdcb282739abbb6c833ae95fd52f93c427b83
jamesawp1 <jamesawp@protonmail.com>
2025-05-22 17:37:56 -0300

    feat: add users controller

---
+++ controllers/users.js
@@ -0,0 +1,13 @@
+const User = require("../models/user");
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch((err) => res.status(404).send({ message: err.message }));
+};
+
+module.exports.getUserById = (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(404).send({ message: err.message }));
+};
