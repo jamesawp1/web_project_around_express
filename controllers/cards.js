ce3c71f7f4806cc32e81555d4463fd867546aa00
jamesawp1 <jamesawp@protonmail.com>
2025-05-23 11:24:41 -0300

    refactor: card controller error handling

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,26 +1,62 @@
 const Card = require("../models/card");
 
+const INVALID_DATA = 400;
+const DATA_NOT_FOUND = 404;
+const SERVER_ERROR = 500;
+
 module.exports.getCards = (req, res) => {
   Card.find({})
     .populate("user")
-    .then((films) => res.send({ data: films }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((films) => {
+      if (!films) {
+        return res
+          .status(DATA_NOT_FOUND)
+          .send({ message: `Cartões não encontrados.` });
+      }
+      return res.send({ data: films });
+    })
+    .catch((err) => {
+      res
+        .status(SERVER_ERROR)
+        .send({ message: `Não foi possível obter os cartões. ERRO: ${err}` });
+    });
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  console.log(`ERROOOO ${req.user._id}`);
   const owner = req.user._id;
 
   Card.create({ name, link, owner })
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((card) => {
+      if (!card.name || !card.link) {
+        return res.status(INVALID_DATA).send({
+          message: `O nome ou o link do cartão é inválido. Nome: ${card.name} e Link: ${card.link}`,
+        });
+      }
+      return res.send({ data: card });
+    })
+    .catch((err) => {
+      res
+        .status(SERVER_ERROR)
+        .send({ message: `Não foi possível criar o cartão. ERRO: ${err}` });
+    });
 };
 
 module.exports.deleteCard = (req, res) => {
   const { id: cardId } = req.params;
 
   Card.findByIdAndDelete(cardId)
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ data: err.message }));
+    .then((card) => {
+      if (!card) {
+        return res.status(DATA_NOT_FOUND).send({
+          message: `Cartão não encontrado. Id do cartão solicitado: ${cardId}.`,
+        });
+      }
+      return res.send({ data: card });
+    })
+    .catch((err) => {
+      res.status(SERVER_ERROR).send({
+        data: `Não foi possível excluir o cartão. ERRO: ${err}`,
+      });
+    });
 };
