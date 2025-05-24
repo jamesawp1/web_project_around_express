b47b91abfe89c6532a9297f6e6040d3a746fd609
jamesawp1 <jamesawp@protonmail.com>
2025-05-24 16:09:47 -0300

    feature: Implement the other methods to the routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,21 @@
 const cardsRouter = require("express").Router();
 
-const { getCards, createCard, deleteCard } = require("../controllers/cards");
+const {
+  getCards,
+  createCard,
+  deleteCard,
+  likeCard,
+  dislikeCard,
+} = require("../controllers/cards");
 
 cardsRouter.get("/", getCards);
 
 cardsRouter.post("/", createCard);
 
 cardsRouter.delete("/:cardId", deleteCard);
+
+cardsRouter.put("/:cardId/likes", likeCard);
+
+cardsRouter.delete("/:cardId/likes", dislikeCard);
 
 module.exports = { cardsRouter };
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,21 @@
 const usersRouter = require("express").Router();
 
-const { getUsers, getUserById, createUser } = require("../controllers/users");
+const {
+  getUsers,
+  getUserById,
+  createUser,
+  updateProfileInfo,
+  updateProfileAvatar,
+} = require("../controllers/users");
 
 usersRouter.get("/", getUsers);
 
 usersRouter.get("/:userId", getUserById);
 
 usersRouter.post("/", createUser);
+
+usersRouter.patch("/me", updateProfileInfo);
+
+usersRouter.patch("/me/avatar", updateProfileAvatar);
 
 module.exports = { usersRouter };
